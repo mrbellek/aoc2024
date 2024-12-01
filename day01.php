a3a7fed2cd1066be6f5de8d891214ba08d43247b
mrbellek <mrbellek@gmail.com>
2024-12-01 20:34:52 +0100

    first commit lol

---
+++ day01.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+class AocDay01
+{
+    public function __construct()
+    {
+       
+    }
+       
+    public function run(): void
+    {
+       
+    }
+}
+
+(new AocDay01)->run();
