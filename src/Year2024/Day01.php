cd08456e2d1d2a9a54c1049d4cd68850acc32acc
mrbellek <mrbellek@gmail.com>
2024-12-07 09:37:15 +0100

    moved solutions to day 1+2 into framework

--- src/Year2024/Day01.php
+++ src/Year2024/Day01.php
@@ -8,6 +8,9 @@ use AdventOfCode\AbstractDay;
 
 class Day01 extends AbstractDay
 {
+    private const SEP = '   ';
+    private array $rightCounts = [];
+
     public function __construct(string $dataSet)
     {
         parent::__construct($dataSet);
@@ -15,11 +18,38 @@ class Day01 extends AbstractDay
 
     public function part1(): void
     {
-        echo 'hello2!';
+        [$left, $right] = $this->getLists();
+        sort($left);
+        sort($right);
+
+        $totalDistance = 0;
+        for ($i = 0; $i < count($left); $i++) {
+            $totalDistance += abs($left[$i] - $right[$i]);
+        }
+
+        printf('Total distance: %d' . PHP_EOL, $totalDistance);
     }
 
     public function part2(): void
     {
+        $similarityScore = 0;
+        [$left, $right] = $this->getLists();
+        $this->rightCounts = array_count_values($right);
+        foreach ($left as $leftNum) {
+            $count = $this->rightCounts[$leftNum] ?? 0;
+            $similarityScore += $leftNum * $count;
+        }
 
+        printf('Similarity score: %s' . PHP_EOL, $similarityScore);
+    }
+
+    private function getLists(): array
+    {
+        $left = [];
+        foreach ($this->input as $line) {
+            [$left[], $right[]] = explode(self::SEP, $line);
+        }
+
+        return [$left, $right];
     }
 }
