82170353eddeef9f56d8da7955d79245180ab81c
mrbellek <mrbellek@gmail.com>
2024-12-17 19:42:00 +0100

    rewrote Day09 solution, still wrong answer :(

--- src/Year2024/Day09.php
+++ src/Year2024/Day09.php
@@ -10,69 +10,60 @@ class Day09 extends AbstractDay
 {
     public function part1(): void
     {
-        $this->log('Converting input to block string..');
-        $blockString = $this->convertMapToBlockString($this->input[0]);
+        $blockString = $this->convertInputToBlockString($this->input[0]);
+        $this->log(implode('', $blockString));
 
-        $this->log('Defragmenting..');
-        for ($i = 0; $i < strlen($blockString); $i++) {
-            if ($this->isLive) {
-                if ($i % 1000 === 0) {
-                    echo '.';
-                }
-            } else {
-                $this->debug($blockString);
+        //defrag
+        $length = count($blockString);
+        for ($i = $length - 1; $i >= 0; $i--) {
+            $char = $blockString[$i];
+            if ($char === '.') {
+                continue;
             }
-            $rightCharPos = $this->findRightMostChar($blockString);
-            $rightChar = substr($blockString, $rightCharPos, 1);
-            $firstDot = strpos($blockString, '.');
-            
-            $blockString[$firstDot] = $rightChar;
-            $blockString[$rightCharPos] = '.';
-            
-            if (preg_match('/^[A-Za-z0-9]+\.+$/', $blockString) === 1) {
+
+            $firstDotPos = array_search('.', $blockString, true);
+            if ($firstDotPos === false) {
+                $this->debug($blockString);
+                die('no dot found?!');
+            }
+
+            $charLen = strlen($char);
+            for ($j = 0; $j < $charLen; $j++) {
+                $blockString[$firstDotPos + $j] = $char[$j];
+            }
+            $blockString[$i] = str_repeat('.', strlen($char));
+
+            $this->debug(implode('', $blockString));
+            if (!$this->isLive) {
+                usleep(100_000);
+            }
+
+            //stop when first dot is beyond cursor
+            if (in_array('.', array_slice($blockString, 0, $i), true) === false) {
                 break;
             }
         }
 
-        $this->debug($blockString);
-        $this->log(PHP_EOL . 'Done!');
-
-        $checksum = 0.0;
-        for ($i = 0; $i < strlen($blockString); $i++) {
-            $checksum += (float) ((float) $i * (float) $blockString[$i]);
+        //checksum
+        $checksum = 0;
+        for ($i = 0; $i < $length; $i++) {
+            $checksum += $i * (int)$blockString[$i];
         }
-
-        $this->log(sprintf('The checksum is %s', $checksum));
+        $this->log(sprintf('Done! Checksum is %d.', $checksum));
     }
 
-    public function part2(): void
+    private function convertInputToBlockString(string $input): array
     {
-        die('unfinished!');
-    }
+        $blocks = array_values(array_filter(str_split($input), static fn(string $index) => $index % 2 === 0, ARRAY_FILTER_USE_KEY));
+        $frees = array_values(array_filter(str_split($input), static fn(string $index) => $index % 2 === 1, ARRAY_FILTER_USE_KEY));
 
-    private function convertMapToBlockString(string $map): string
-    {
-        $blockString = '';
-        $mapArray = str_split($map);
-        $files = array_values(array_filter($mapArray, fn (int $index) => $index % 2 === 0, ARRAY_FILTER_USE_KEY));
-        $spaces = array_values(array_filter($mapArray, fn (int $index) => $index % 2 === 1, ARRAY_FILTER_USE_KEY));
-      
-        for ($i = 0; $i < max(count($files), count($spaces)); $i++) {
-            $blockString .= str_repeat((string) $i, (int) $files[$i] ?? 0);
-            $blockString .= str_repeat('.', (int) ($spaces[$i] ?? 0));
+        $blockString = [];
+        $max = count($blocks);
+        for ($i = 0; $i < $max; $i++) {
+            array_push($blockString, ...array_fill(0, (int)$blocks[$i], (string)$i));
+            array_push($blockString, ...array_fill(0, (int)($frees[$i] ?? 0), '.'));
         }
-        $this->log(sprintf('Converted input of %d bytes into blockstring of length %d', strlen($map), strlen($blockString)));
-        
+
         return $blockString;
-    }
-
-    private function findRightMostChar(string $blockString): int
-    {
-        $m = [];
-        if (preg_match('/[A-Za-z0-9]/', strrev($blockString), $m, PREG_OFFSET_CAPTURE) === 1) {
-            return strlen($blockString) - $m[0][1] - 1;
-        }
-        
-        throw new RuntimeException('wtf');
     }
 }
