f662548eb0bc48f76f210aefa581866caecdddda
mrbellek <mrbellek@gmail.com>
2025-01-12 16:53:13 +0100

    solved 2015-11

--- src/Year2015/Day11.php
+++ src/Year2015/Day11.php
@@ -8,11 +8,14 @@ use  AdventOfCode\AbstractDay;
 
 class Day11 extends AbstractDay
 {
+    public const PART1_COMPLETE = true;
+    public const PART2_COMPLETE = true;
+
     public function part1(): void
     {
-        die(var_dump($this->getNextPassword('abcxyz')));
         if ($this->isLive === true) {
             $password = $this->input[0];
+            $this->log(sprintf('Looking for next password after "%s"...', $password));
             while ($this->isValidPassword($password) === false) {
                 $password = $this->getNextPassword($password);
             }
@@ -36,15 +39,60 @@ class Day11 extends AbstractDay
                 'ghijklmn',
             ];
             foreach ($nextPasswords as $password) {
+                $oldPassword = $password;
+                $password = $this->getNextPassword($password);
+                while ($this->isValidPassword($password) === false) {
+                    $password = $this->getNextPassword($password);
+                }
                 $this->debug(sprintf('The next password after %s is %s',
-                    $password,
-                    $this->getNextPassword($password)
+                    $oldPassword,
+                    $password
                 ));
             }
         }
     }
 
+    public function part2(): void
+    {
+        if ($this->isLive === true) {
+            $password = $this->input[0];
+            $this->log(sprintf('Looking for next password after "%s"...', $password));
+            while ($this->isValidPassword($password) === false) {
+                $password = $this->getNextPassword($password);
+            }
+            $this->log(sprintf('New password: %s', $password));
+
+            $this->log(sprintf('Looking for next password after "%s"...', $password));
+            $password = $this->getNextPassword($password);
+            while ($this->isValidPassword($password) === false) {
+                $password = $this->getNextPassword($password);
+            }
+            $this->log(sprintf('New password: %s', $password));
+        } else {
+            $this->fatal('no sample input for part2.');
+        }
+    }
+
     private function isValidPassword(string $password): bool
+    {
+        return $this->hasNoIOL($password) && $this->hasStraight($password) && $this->hasPairs($password);
+    }
+
+    private function hasNoIOL(string $password): bool
+    {
+        //Password may not contain i, o, l
+        if (
+            str_contains($password, 'i') ||
+            str_contains($password, 'o') ||
+            str_contains($password, 'l')
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    private function hasStraight(string $password): bool
     {
         //Password must include one increasing straight
         //of at last three letters.
@@ -62,76 +110,42 @@ class Day11 extends AbstractDay
             }
         }
         if ($incrementPresent === false) {
-            $this->debug(sprintf('password %s invalid because no increasing 3-straight', $password));
             return false;
-        }
-
-        //Password may not contain i, o, l
-        if (
-            str_contains($password, 'i') ||
-            str_contains($password, 'o') ||
-            str_contains($password, 'l')
-        ) {
-            $this->debug(sprintf('password %s invalid because it has i/o/l', $password));
-            return false;
-        }
-
-        if (preg_match_all('/(\w)\1/', $password, $m) > 1) {
-            if (count($m[0]) < 2) {
-                $this->debug(sprintf('password %d invalid because no two pairs', $password));
-                return false;
-            }
         }
 
         return true;
     }
 
+    private function hasPairs(string $password): bool
+    {
+        $m = [];
+        if (preg_match_all('/(\w)\1/', $password, $m) > 1) {
+            if (count($m[0]) >= 2) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     private function getNextPassword(string $password): string
     {
-        /**
-         * TODO:
-         * - convert password to base-26 'numbers', i.e. substract 10 from everything
-         * - convert to decimal
-         * - add 1
-         * - convert back to base-26
-         * - add 10 to everything
-         */
-        $table = [
-            'a' => '0',
-            'b' => '1',
-            'c' => '2',
-            'd' => '3',
-            'e' => '4',
-            'f' => '5',
-            'g' => '6',
-            'h' => '7',
-            'i' => '8',
-            'j' => '9',
-        ];
-        $temp = '';
-        for ($i = 0; $i < strlen($password); $i++) {
-            if (ord($password[$i]) > 106) {
-                //result is a letter
-                $temp .= chr(ord($password[$i]) - 10);
-            } else {
-                //result has to be a number
-                $temp .= $table[$password[$i]];
-            }
+        //the answer of course, is recursion
+        return $this->increment($password, strlen($password) - 1);
+    }
+
+    private function increment(string $s, int $index): string
+    {
+        if (substr($s, $index, 1) !== 'z') {
+            //php refuses to increment a letter if it's not in a separate var
+            $char = $s[$index];
+            $char++;
+            $s[$index] = $char;
+
+            return $s;
+        } else {
+            $s[$index] = 'a';
+            return $this->increment($s, $index - 1);
         }
-
-        $temp = base_convert(strval(base_convert($temp, 26, 10) + 1), 10, 26);
-        $this->debug($temp);
-
-        $table = array_flip($table);
-        $temp2 = '';
-        for ($i = 0; $i < strlen($temp); $i++) {
-            if (is_numeric($temp[$i])) {
-                $temp2 .= $table[$temp[$i]];
-            } else {
-                $temp2 .= chr(ord($temp[$i]) + 10);
-            }
-        }
-
-        return $temp2;
     }
 }
