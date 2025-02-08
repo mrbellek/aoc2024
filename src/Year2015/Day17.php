9cd41cc75f6acc2211503ba385d79d4ac328f3a7
mrbellek <mrbellek@gmail.com>
2025-02-08 17:38:32 +0100

    working on 2015-17-1, still tripping over nonunique buckets

---
+++ src/Year2015/Day17.php
@@ -0,0 +1,63 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AdventOfCode\Year2015;
+
+use AdventOfCode\AbstractDay;
+use AdventOfCode\Traits\PermutationTrait;
+
+class Day17 extends AbstractDay
+{
+    use PermutationTrait;
+
+    private array $containers;
+    private int $eggnogVolume = 150;
+
+    public function part1(): void
+    {
+        if ($this->isLive === false) {
+            $this->eggnogVolume = 25;
+        }
+
+        foreach (array_filter($this->input) as $i => $volume) {
+            $this->containers[$i] = ['index' => $i, 'volume' => $volume];
+        }
+
+        $containerSets = [];
+        foreach ($this->getPermutations($this->containers) as $containers) {
+            $num = $this->fitsNumContainersExactly($containers);
+            if ($num > 0) {
+                $containerSet = array_column(array_slice($containers, 0, $num), 'volume');
+                sort($containerSet);
+                $containerSets[] = implode(',', $containerSet);
+            }
+        }
+
+        $containerSets = array_unique($containerSets);
+        $this->log(sprintf('There are %d combinations of the given containers that fit %d L eggnog',
+            count($containerSets),
+            $this->eggnogVolume
+        ));
+        foreach ($containerSets as $containerSet) {
+            $this->debug($containerSet);
+        }
+    }
+
+    private function fitsNumContainersExactly(array $containers): int
+    {
+        $remainingVolume = $this->eggnogVolume;
+        $numContainers = 0;
+        foreach ($containers as $container) {
+            $remainingVolume -= (int)$container['volume'];
+            $numContainers++;
+            if ($remainingVolume === 0) {
+                return $numContainers;
+            } elseif ($remainingVolume < 0) {
+                return 0;
+            }
+        }
+
+        return 0;
+    }
+}
